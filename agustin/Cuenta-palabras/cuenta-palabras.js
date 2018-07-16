e65d57044fb0cf7d14638a14ad38f9d79bc0cb87
agusDev <curieltapiaagustin0112@gmail.com>
2018-07-16 11:03:08 -0500

    ejercicios

---
+++ agustin/Cuenta-palabras/cuenta-palabras.js
@@ -0,0 +1,18 @@
+let cadena = "hola amigos, el otro día me encontré con mis amigos, que conocían a sus amigos"
+
+function cuentaPalabras (palabra, frase) {
+    let pos = 0
+    let cont = 0
+    while (pos > -1) {
+        pos = frase.indexOf(palabra, pos)
+        if (pos >= 0) {
+            cont++
+            pos++
+        }
+    }
+    console.log(cont)
+    return cont //cuantas palabras hay en la cadena
+}
+
+console.log('con palabra amigos',cuentaPalabras('amigos', cadena))
+console.log('con ,',cuentaPalabras(',', cadena))
